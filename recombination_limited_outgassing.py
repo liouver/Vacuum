9f869c0dac7186ff5d4f22d803b64e29bbef785e
Wei <weiliu1006@yahoo.com>
2017-12-15 22:35:25 +0800

    update

--- recombination_limited_outgassing.py
+++ recombination_limited_outgassing.py
@@ -27,12 +27,10 @@ def compute_u_time(u, D, h, dt, K, S):
     using central differences with spacing h ,
     and return g (x , t ).
     Then compute the solution for u after t, u = u + g * dt"""
-
     # # # boundary condtion
     u[0] = (-D + np.sqrt(D**2 + 4 * D * K * h * u[1])) / (2 * K * h)
     u[-1] = (-D + np.sqrt(D**2 + 4 * D * K * h * (u[-2] - S))) / (2 * K * h)\
         + S
-
     # # # within stainless steel
     d2u_dx2 = np.zeros(u. shape, np . float)
     for i in range(1, len(u) - 1):
@@ -42,25 +40,91 @@ def compute_u_time(u, D, h, dt, K, S):
     return u
 
 
-def compute_outgass(u, D, h):
-    du_dx_0 = (u[1] - u[0]) / (h)
-    return D * du_dx_0
-
-
-def time_dependent_outgass(D, time, C0, K, S):
+def compute_concentration(D, time, C0, K, S):
     ''' Given the time to be calculated, compute the outgassing rate q(t),
      and the hydrogen concentration u(x ,t) '''
     u = C0 * np.ones(np.size(x))
     steps = 1 / dt
     u_line = np.arange(0, time).reshape(time, 1)
     u_data = u * u_line
-    q = []
     for i in range(time):
         for j in range(int(steps)):
             u = compute_u_time(u, D, h, dt, K, S)
-        q.append(compute_outgass(u, D, h))
         u_data[i, :] = u
-    return q, u_data
+    return u_data
+
+
+def compute_outgass_T(D, time, C0, K, S, T):
+    q_data = T * np.linspace(0, time, time).reshape(time, 1)
+    for i in range(len(T)):
+        u = compute_concentration(D[i], time, C0, K[i], S[i])
+        q_data[:, i] = D[i] * (u[:, 1] - u[:, 0]) / h  # atom/(s*cm**2)
+        q_data[:, i] = q_data[:, i] * R * T[i] / (133.322 * 6.022 * 10**20)
+    return q_data
+
+
+def save_data(q_data, u_data):
+    q_data = q_data * 10**10
+    len1 = np.size(q_data[:, 0])
+    N = int(20 * np.log10(len1))
+    file1 = open('outgassing.txt', 'w+')
+    var = -1
+    file1.write('%.1f ' % 0)
+    for j in range(np.size(q_data[0, :])):
+        file1.write('%.2f ' % q_data[0, j])
+    file1.write('\n')
+    for i in range(N + 1):
+        i = int(10**(i / 20))
+        if (i != var):
+            file1.write('%.1f ' % i)
+            for j in range(np.size(q_data[0, :])):
+                file1.write('%.4f ' % q_data[i, j])
+            file1.write('\n')
+        var = i
+    file1.close()
+    len2 = np.size(u_data[:, 0])
+    file2 = open('concentration.txt', 'w+')
+    file2.write('%.1f ' % 0)
+    for j in range(np.size(u_data[0, :])):
+        file2.write('%.1f ' % u_data[0, j])
+    file2.write('\n')
+    for a in range(int(np.log10(len2))):
+        for b in range(9):
+            i = (b + 1) * 10**a
+            file2.write('%.1f ' % i)
+            for j in range(np.size(u_data[0, :])):
+                file2.write('%.1f ' % u_data[i, j])
+            file2.write('\n')
+    len2 = len2 - 1
+    file2.write('%.1f' % len2)
+    for j in range(np.size(u_data[0, :])):
+        file2.write('%.1f ' % u_data[len2, j])
+    file2.write('\n')
+    file2.close()
+
+
+def plot_outgassing(t, q_data):
+    plt.figure()
+    plt.loglog(t, q_data[:, 0], t, q_data[:, 1], t, q_data[:, 2],
+               t, q_data[:, 3], t, q_data[:, 4], t, q_data[:, 5])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Outgassing rate ($torr \cdot L \cdot s^{-1} \cdot cm^{-2}$)')
+    plt.legend(['100$^\circ$', '250$^\circ$', '400$^\circ$', '600$^\circ$',
+                '800$^\circ$', '950$^\circ$', '100$^\circ$'],
+               loc='best', fontsize=12, frameon=False)
+    plt.title('Thickness = 0.3 cm')
+
+
+def plot_concentration(x, u_data):
+    plt.figure()
+    plt.plot(x, u_data[1, :], x, u_data[10, :], x, u_data[100, :], x, u_data[
+             50, :], x, u_data[10, :], x, u_data[50, :])
+    plt.xlim([0, 0.3])
+    plt.xlabel('depth (cm)')
+    plt.ylabel('Hydrogen concentration ($atom \cdot cm^{-3}$)')
+    plt.legend(['1 s', '10 s', '100 s', '500 s', '1000 s', '5000 s'],
+               loc='best', fontsize=12, frameon=False)
+    plt.title('Temperature = 950$^\circ$')
 
 
 def main():
@@ -79,33 +143,13 @@ def main():
     #  Initial concentration, Ref: J. Vac. Sci. Technol. A 13, 545 (1995)
     # C0 = 0.3 torr * L @ 273 K / cm**3
     C0 = 0.3 * 133.322 * 6.022 * 10**20 / (273 * R)  # atom/cm**3
-    print(D, K)
-    q_data = T * np.linspace(0, time, time).reshape(time, 1)
-    for i in range(np.size(T)):
-        q, u_data = time_dependent_outgass(D[i], time, C0, K[i], S[i])
-        q_data[:, i] = np.array(q) * R * T[i] / (133.322 * 6.022 * 10**20)
-    q, u_data = time_dependent_outgass(D[5], time, C0, K[5], S[5])
-    u_data = u_data * R * T[5] / (133.322 * 6.022 * 10**20)  # torr*L/cm**3
 
-    plt.figure()
-    plt.loglog(t, q_data[:, 0], t, q_data[:, 1], t, q_data[:, 2],
-               t, q_data[:, 3], t, q_data[:, 4], t, q_data[:, 5])
-    plt.xlabel('Time (s)')
-    plt.ylabel('Outgassing rate ($torr \cdot L \cdot s^{-1} \cdot cm^{-2}$)')
-    plt.legend(['100$^\circ$', '250$^\circ$', '400$^\circ$', '600$^\circ$',
-                '800$^\circ$', '950$^\circ$', '100$^\circ$'],
-               loc='best', fontsize=12, frameon=False)
-    plt.title('Thickness = 0.3 cm')
-
-    plt.figure()
-    plt.plot(x, u_data[1, :], x, u_data[10, :], x, u_data[100, :], x, u_data[
-             50, :], x, u_data[10, :], x, u_data[50, :])
-    plt.xlim([0, 0.3])
-    plt.xlabel('depth (cm)')
-    plt.ylabel('Hydrogen concentration ($torr \cdot L \cdot cm^{-3}$)')
-    plt.legend(['1 s', '10 s', '100 s', '500 s', '1000 s', '5000 s'],
-               loc='best', fontsize=12, frameon=False)
-    plt.title('Temperature = 950$^\circ$')
+    q_data = compute_outgass_T(D, time, C0, K, S, T)
+    u_data = compute_concentration(D[5], time, C0, K[5], S[5])  # atom/cm**3
+#     u_data = u_data * R * T[5] / (133.322 * 6.022 * 10**20)  # torr*L/cm**3
+#    save_data(q_data, u_data)
+    plot_outgassing(t, q_data)
+    plot_concentration(x, u_data)
     plt.show()
 
 
